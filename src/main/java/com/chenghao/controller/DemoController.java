d333c62f2ed9c4bc9334fc21e961783cf5c703ea
hao.chenghao <Hao5211314>
2020-09-03 10:03:38 +0800

    初始

---
+++ src/main/java/com/chenghao/controller/DemoController.java
@@ -0,0 +1,21 @@
+package com.chenghao.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @ClassName DemoController
+ * @Description
+ * @Author hao.chenghao
+ * @Date 2020/9/3 9:49
+ * @Version 1.0
+ */
+@RestController
+public class DemoController {
+
+
+    @GetMapping("/getSetting")
+    public String setting() {
+        return "111111";
+    }
+}
